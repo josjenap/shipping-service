b3c95b12c987758acd227748659262680391ccf3
Josje Nap <josjenap@nl.ibm.com>
2018-11-01 19:08:52 +0100

    This reverts commit 22facf68695c35b7990f9011d0155fd74bfcf03f, reversing changes made to e864e8d2b8e0472c4bf6df171a60e53f5e7a4561.
    d    node_modules/.bin/mime

--- src/controllers/shipping-controller.js
+++ src/controllers/shipping-controller.js
@@ -1,4 +1,3 @@
-
 // src/controllers/shipping-controller.js
 var productService = require('../services/product-service')
 
@@ -11,15 +10,7 @@ class ShippingController {
 
   async getItemShipping(item) {
     var shippingAmount = await productService.getProductWeight(item.id)
-<<<<<<< HEAD
     return shippingAmount * this.REGULAR_PRICE
-=======
-    if (item.type.toLowerCase() === 'overnight') {
-      return shippingAmount * this.OVERNIGHT_PRICE
-    } else {
-      return shippingAmount * this.REGULAR_PRICE
-    }
->>>>>>> c5342a566a8262dd36d87e99b039e5dcf65dd1ec
   }
 
 }
--- tests/shipping-controller.test.js
+++ tests/shipping-controller.test.js
@@ -1,16 +1,11 @@
 // tests/shipping-controller.test.js
 var chai = require('chai')
 var expect = chai.expect
-<<<<<<< HEAD
 var sinon = require('sinon');
-=======
-var sinon = require('sinon')
->>>>>>> c5342a566a8262dd36d87e99b039e5dcf65dd1ec
 var ShippingController = require('../src/controllers/shipping-controller')
 var productService = require('../src/services/product-service')
 
 describe('Shipping controller', function () {
-<<<<<<< HEAD
     var shippingCtrl = new ShippingController()
   
     beforeEach(function(){
@@ -33,31 +28,3 @@ describe('Shipping controller', function () {
     })
   
   })
-=======
-  var shippingCtrl = new ShippingController()
-
-  beforeEach(function(){
-    sinon.stub(productService, 'getProductWeight').callsFake(async function() {
-      return new Promise(function (resolve, reject) {
-        setTimeout(() => {
-          resolve(5)
-        }, 50)
-      })
-    })
-  })
-
-  afterEach(function () {
-    productService.getProductWeight.restore()
-  })
-
-  it('Should calculate correct shipping ', async function () {
-    let shipping = await shippingCtrl.getItemShipping({ id: 1, type: 'standard' })
-    expect(shipping).to.equal(0.5)
-  })
-
-  it('Should calculate correct overnight shipping ', async function () {
-    let shipping = await shippingCtrl.getItemShipping({ id: 1, type: 'overnight' })
-    expect(shipping).to.equal(5)
-  })
-})
->>>>>>> c5342a566a8262dd36d87e99b039e5dcf65dd1ec
